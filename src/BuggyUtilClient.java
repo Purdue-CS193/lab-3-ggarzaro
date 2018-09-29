772272ae0be38490be9a88e18f6cfb15171ac877
ggarzaro <42808181+ggarzaro@users.noreply.github.com>
2018-09-28 19:54:19 -0600

    Update BuggyUtilClient.java

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -1,31 +1,34 @@
-
 import java.util.Scanner;
 
 public class BuggyUtilClient {
-    public static void main(String[] args) {
+
+    public static void main (String [] arags){
         // First test case in Main
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("Enter First Name:");
 
-        String FirstName = scanner.nextLine();
+        String firstName = scanner.nextLine();
 
         System.out.println("Enter Last Name:");
 
-        String LastName = scanner.nextLine();
+        String lastName = scanner.nextLine();
 
-        String FN = FirstName.substring(0,1);
+        String FN = firstName.substring(0,1);
         String LN;
 
-        if (LastName.length() > 8) {
-            LN = LastName.substring(0, 7);
+        if (lastName.length() > 8) {
+            LN = lastName.substring(0, 7);
         } else {
-            LN = LastName;
+            LN = lastName;
         }
-     
-        System.out.println("Out: "+FN+LN);
+        String Username = FN+LN;
+        String res = Username.toLowerCase();
+
+        System.out.println("Out: "+res);
+       
 
     }
+    
 }
-
 
